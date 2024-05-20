9e916d85d12a125017c25bc921802bbd3fd8b37e
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 13:21:10 +0800

    Update home-page.js

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Auth0Features } from "../components/auth0-features";
 import { HeroBanner } from "../components/hero-banner";
 import { PageLayout } from "../components/page-layout";
-import { ImageScrolling } from "../components/ImageScrolling";
 
 export const HomePage = () => (
   <PageLayout>
     <HeroBanner />
     <Auth0Features />
-    <ImageScrolling />
   </PageLayout>
 );
