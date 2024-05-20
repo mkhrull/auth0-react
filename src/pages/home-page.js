21382850676a789fe32717ec398fb0f9a9624013
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 13:48:11 +0800

    Update home-page.js

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,14 +2,10 @@ import React from "react";
 import { Auth0Features } from "../components/auth0-features";
 import { HeroBanner } from "../components/hero-banner";
 import { PageLayout } from "../components/page-layout";
-import "../styles/HomePage.css";
 
 export const HomePage = () => (
   <PageLayout>
     <HeroBanner />
     <Auth0Features />
-    <div className="hello-world-container">
-      <h1 className="hello-world-text">Hello World!</h1>
-    </div>
   </PageLayout>
 );
