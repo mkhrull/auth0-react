57ecdd9973cc638e703429416978ea19eea8ad66
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 15:16:10 +0800

    Update InfiniteScrolling.js

--- src/components/InfiniteScrolling.js
+++ src/components/InfiniteScrolling.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 export const InfiniteScrolling = () => {
   const containerRef = useRef(null);
