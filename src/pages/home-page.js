856a5420a4c462595cc139dc7d3ea59d94a45321
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 13:36:11 +0800

    Update home-page.js

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Auth0Features } from "../components/auth0-features";
 import { HeroBanner } from "../components/hero-banner";
 import { PageLayout } from "../components/page-layout";
-import "./HomePage.css";
+import "../styles/HomePage.css";
 
 export const HomePage = () => (
   <PageLayout>
