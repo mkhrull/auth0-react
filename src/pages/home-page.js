b579bfa551805b11c2d92cb6f5d94016ad32ed5c
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 13:34:16 +0800

    Update home-page.js

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Auth0Features } from "../components/auth0-features";
 import { HeroBanner } from "../components/hero-banner";
 import { PageLayout } from "../components/page-layout";
+import "./HomePage.css";
 
 export const HomePage = () => (
   <PageLayout>
     <HeroBanner />
     <Auth0Features />
-     <div>
-      <h1 style={{ color: "white" }}>Hello World!</h1>
+    <div className="hello-world-container">
+      <h1 className="hello-world-text">Hello World!</h1>
     </div>
   </PageLayout>
 );
