84be57762092679ef21f204520e804e77ee4499b
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 15:46:24 +0800

    Update InfiniteScrolling.js

--- src/components/InfiniteScrolling.js
+++ src/components/InfiniteScrolling.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
+import './InfiniteScrolling.css'; // Import the CSS file for styling
 
-export const InfiniteScrolling = () => {
+default const InfiniteScrolling = () => {
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -25,29 +26,21 @@ export const InfiniteScrolling = () => {
     <div
       className="infinite-scrolling"
       ref={containerRef}
-      style={{
-        display: "flex",
-        overflowX: "auto",
-        overflowY: "hidden",
-        whiteSpace: "nowrap",
-        scrollBehavior: "smooth",
-      }}
     >
       <img
         src="https://khaihsn.xyz/wp-content/uploads/2024/05/CFD507AF-FC0D-45E2-9520-2E850305253B-scaled-e1715490491979.jpeg"
         alt="Infinite Scrolling"
-        style={{
-          height: "200px",
-          marginRight: "20px",
-        }}
+        className="scrolling-image"
       />
       <img
         src="https://khaihsn.xyz/wp-content/uploads/2024/05/CFD507AF-FC0D-45E2-9520-2E850305253B-scaled-e1715490491979.jpeg"
         alt="Infinite Scrolling"
+        className="scrolling-image"
       />
       <img
         src="https://khaihsn.xyz/wp-content/uploads/2024/05/CFD507AF-FC0D-45E2-9520-2E850305253B-scaled-e1715490491979.jpeg"
         alt="Infinite Scrolling"
+        className="scrolling-image"
       />
       {/* Add more images as needed */}
     </div>
