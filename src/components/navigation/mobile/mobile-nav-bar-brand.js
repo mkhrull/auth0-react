b55ce51b7ba25e19083fa39de75bdfabb7fccc11
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-20 11:10:22 +0800

    Update mobile-nav-bar-brand.js

--- src/components/navigation/mobile/mobile-nav-bar-brand.js
+++ src/components/navigation/mobile/mobile-nav-bar-brand.js
@@ -7,7 +7,7 @@ export const MobileNavBarBrand = ({ handleClick }) => {
       <NavLink to="/">
         <img
           className="mobile-nav-bar__logo"
-          src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
+          src="https://khaihsn.xyz/wp-content/uploads/2024/05/coding.png"
           alt="Auth0 shield logo"
           width="82"
           height="24"
